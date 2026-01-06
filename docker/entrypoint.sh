04ca2068658a5f5744fc81551fa274acb5431ae8
mohanakrishna sadhu <43145647+AxcelBlaze003@users.noreply.github.com>
2026-01-06 15:18:26 +0530

    fix: Added support for Read only file system

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -13,11 +13,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+WRITABLE_CACERTS="/java-security/cacerts"
+ORIGINAL_CACERTS="${JAVA_HOME}/lib/security/cacerts"
+
+echo "Copying system cacerts to writable space..."
+cp "${ORIGINAL_CACERTS}" "${WRITABLE_CACERTS}"
+
+export JAVA_TOOL_OPTIONS="${JAVA_TOOL_OPTIONS} -Djavax.net.ssl.trustStore=${WRITABLE_CACERTS} -Djavax.net.ssl.trustStorePassword=changeit"
 set +e
 if [[ "$(ls ${TRUST_CERTS_DIR})" ]]; then
     for filename in ${TRUST_CERTS_DIR}/*; do
         echo "Import $filename certificate to Java cacerts"
-        ${JAVA_HOME}/bin/keytool -import -trustcacerts -keystore ${JAVA_HOME}/lib/security/cacerts -storepass changeit -noprompt -alias ${filename} -file ${filename}
+        ${JAVA_HOME}/bin/keytool -import -trustcacerts -keystore "${WRITABLE_CACERTS}" -storepass changeit -noprompt -alias "$(basename ${filename})" -file "${filename}"
     done;
 fi
 set -e
